81400f077e03d283acb7cb55da4bfdcd21d79f02
David Chai <davidchai717@gmail.com>
2019-12-24 13:43:52 +0800

    Items ported to controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const items = fs.readFileSync(path.resolve(__dirname, '../models/items.json'));
+
+console.log(JSON.parse(items));
+
 module.exports = {
   addCart: {
-
+    // access from the store
+    // create a new Cart entry
+    // return cart ID
   },
   getItems: {
-
   },
   addItem: {
 
