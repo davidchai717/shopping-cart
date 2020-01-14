b7e5c682486337d0232726a849df94286aad261d
David Chai <davidchai717@gmail.com>
2020-01-13 21:06:42 -0500

    addCart functionality complete

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const items = require('../models/items');
 
-console.log(items);
+const Cart = require('../models/cart');
 const carts = [];
 
 module.exports = {
-  addCart: {
+  addCart: function () {
     // access from the store
     // create a new Cart entry
-    // return cart ID
+    const newCart = new Cart();
+    // push and return the new length as the ID
+    return carts.push(newCart);
   },
   getItems: {
   },
